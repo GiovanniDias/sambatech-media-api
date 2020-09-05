41a333171522a4a127efa34a2cba6ab055c09f8c
Giovanni Oliveira <giovanni.oliveira@ceuma.br>
2020-09-05 18:12:13 -0300

    test: Test database connection

---
+++ app/tests/test_db.py
@@ -0,0 +1,9 @@
+from flask_sqlalchemy import SQLAlchemy
+
+def test_db_connection(app):
+    db = SQLAlchemy()
+    db.init_app(app)
+    
+    # For sqlite database, if it does not exist, a file 
+    # with its name will be created when execute engine
+    assert db.engine.execute('SELECT 1') is not None
