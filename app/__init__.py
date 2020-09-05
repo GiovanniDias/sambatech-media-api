00a7f1a1f3344bfba2bf115af9d93d8d350ca8a2
Giovanni Oliveira <giovanni.oliveira@ceuma.br>
2020-09-05 12:03:53 -0300

    feat: Add configuration
    - Add Dynaconf package to handle app configuration settings;
    - Configure app.

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,13 @@
 from flask import Flask
+from .extensions import configuration
 
 
 def create_app(**config):
     app = Flask(__name__)
     
+    # Apply settings to app (settings.toml)
+    configuration.init_app(app, **config)
+
     @app.route('/')
     def index():
         return 'This is a sample of SambaTech Media API'
