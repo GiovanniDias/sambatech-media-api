702d5eec6e1be3904de9cd8fee44d83e8a9bd516
Giovanni Oliveira <giovanni.oliveira@ceuma.br>
2020-09-06 14:51:28 -0300

    test: Add medial model and instance fixtures
    - Media model is used to reference its table on db
    - Media instance to test CRUD operations
    - Create and drop test database during test used during test execution.

--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -1,11 +1,12 @@
 import pytest
 from app.extensions import database
+from app.models.media import Media as MediaModel
 from app import create_app
 
 @pytest.fixture(scope="session")
 def app():
     app = create_app(FORCE_ENV_FOR_DYNACONF="testing")
-    with app.app_context():        
+    with app.app_context():
         yield app
 
 
@@ -15,5 +16,23 @@ def client(app):
 
 
 @pytest.fixture
-def db():
-    return database.db
+def db(app):
+    # with app.app_context():
+    database.db.create_all()
+    yield database.db
+    database.db.drop_all()
+
+
+@pytest.fixture
+def Media():
+    return MediaModel
+
+
+@pytest.fixture
+def media():
+    return MediaModel(
+        id = 1,
+        name = "Test video",
+        duration = 100,
+        url="http://samba_videos/test_video.mp4"
+    )
