90b78f3e73923832568e32626f19848c7bb2b266
Giovanni Oliveira <giovanni.oliveira@ceuma.br>
2020-09-06 11:43:17 -0300

    fix: Use db instance from the current app context
    - Removed new db instance inside db test
    - Returned current app from context

--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -1,12 +1,19 @@
 import pytest
+from app.extensions import database
 from app import create_app
 
 @pytest.fixture(scope="session")
 def app():
     app = create_app(FORCE_ENV_FOR_DYNACONF="testing")
-    yield app
+    with app.app_context():        
+        yield app
 
 
 @pytest.fixture
 def client(app):
     return app.test_client()
+
+
+@pytest.fixture
+def db():
+    return database.db
